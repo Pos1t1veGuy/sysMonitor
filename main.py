81d4ef5b12f2d172606fb4385d3a70f640fccc42
Pos1t1veGuy <UriyDoroshenko@ya.ru>
2023-11-24 16:58:34 +0300

    update

--- main.py
+++ main.py
@@ -1,79 +1,14 @@
-from tkinter import Tk, Label, Canvas
-from random import randint
-
+from tkinter import Tk, Label, Canvas, Frame
 import sys, os
-import json
-import subprocess as sb
-import psutil
+from utils import sysInfo, Config
 
 
-def get_disks():
-    partitions = psutil.disk_partitions()
-    disk_info = []
+def default_label(text: str = 'label'):
+    label = Label(root, text=text, fg="white", font=("Helvetica Bold", 12, "bold"))
+    label.configure(background="black")
+    label.pack(side="top", anchor="w")
+    return label
 
-    for partition in partitions:
-        usage = psutil.disk_usage(partition.mountpoint)
-        disk_info.append({
-            'root': partition.device,
-            'total': usage.total,
-            'free': usage.free
-        })
-
-    return disk_info
-
-def get_ram():
-    global x1, xr1
-    if x1 >= 100:
-        xr1 = False
-    elif x1 <= 0:
-        xr1 = True
-    if xr1:
-        x1+=1
-    else:
-        x1-=1
-    return x1
-    #return randint(0,100)
-
-def get_cpu():
-    global x2, xr2
-    if x2 >= 100:
-        xr2 = False
-    elif x2 <= 0:
-        xr2 = True
-    if xr2:
-        x2+=1
-    else:
-        x2-=1
-    return x2
-    #return randint(0,100)
-
-def init_config():
-    global config, debug, root, main_cfg
-
-    if os.path.isfile(config):
-        for key in main_cfg.keys():
-            if not key in read_config().keys():
-                change_config(**main_cfg)
-
-                if debug:
-                    print(f"Initialized base config")
-                break
-    else:
-        change_config(position=[0,0], size='100x120')
-def change_config(**kwargs):
-    global config
-    if os.path.isfile(config):
-        content = json.load(open(config, 'r'))
-    else:
-        content = {}
-
-    for key, value in kwargs.items():
-        content[key] = value
-
-    json.dump(content, open(config, 'w'))
-def read_config():
-    global config
-    return json.load(open(config, 'r'))
 
 def dragwin(event):
     x = root.winfo_pointerx() - _offsetx
@@ -91,35 +26,38 @@ def releasewin(event):
     button_pressed = False
 
 def hotkeys(event):
+    global debug
     if button_pressed and event.keysym == 'Escape':
         root.destroy()
-    if button_pressed and event.keysym == 'Tab':
+    elif button_pressed and event.keysym == 'Tab':
         sys.exit(os.execl(sys.executable, sys.executable, *sys.argv))
-    if button_pressed and event.keysym == 'space':
+    elif button_pressed and event.keysym == 'space':
         save_coordinates(True)
 
 def save_coordinates(event):
-    global config, _offsetx, _offsety, last_pos
-    if read_config()['save_pos'] or event == True:
+    global config, _offsetx, _offsety, last_pos, last_saved_pos
+
+    if config['save_pos'] or event == True:
         _offsetx = root.winfo_pointerx() - root.winfo_rootx() 
         _offsety = root.winfo_pointery() - root.winfo_rooty()
 
-        if last_pos != [root.winfo_rootx(), root.winfo_rooty()]:
-            change_config(position=[root.winfo_rootx(), root.winfo_rooty()])
-            if debug:
-                print(f'Moved to {[root.winfo_rootx(), root.winfo_rooty()]} and saved to config')
-    else:
-        if debug and last_pos != [root.winfo_rootx(), root.winfo_rooty()]:
-            print(f'Moved to {[root.winfo_rootx(), root.winfo_rooty()]}')
+        if last_saved_pos != [root.winfo_rootx(), root.winfo_rooty()]:
+            config.change(position=[root.winfo_rootx(), root.winfo_rooty()])
+            last_saved_pos = [root.winfo_rootx(), root.winfo_rooty()]
+
+    if debug and last_pos != [root.winfo_rootx(), root.winfo_rooty()]:
+        print(f'Moved to {[root.winfo_rootx(), root.winfo_rooty()]}')
+
     last_pos = [root.winfo_rootx(), root.winfo_rooty()]
 
 def create_graph(canvas, percentage, color, y_position, height):
     width = int((root.winfo_width()-99) * percentage / 100)
     canvas.create_rectangle(0, y_position, width, y_position + height, fill=color, outline="")
 
+
 def update_cpuram():
-    ram_percentage = get_ram()
-    cpu_percentage = get_cpu()
+    ram_percentage = info.get_ram()
+    cpu_percentage = info.get_cpu()
 
     ram_label.config(text=f"RAM: {ram_percentage}%")
     cpu_label.config(text=f"CPU: {cpu_percentage}%")
@@ -130,10 +68,10 @@ def update_cpuram():
     cpu_canvas.delete("all")
     create_graph(cpu_canvas, cpu_percentage, "gray", 0, 16)
 
-    root.after(100, update_cpuram)
+    root.after(info.cpuram_update_rate, update_cpuram)
 
 def update_disks():
-    disk_info = get_disks()
+    disk_info = info.get_disks()
 
     for i, disk in enumerate(disk_info):
         used = (disk['total'] - disk['free'])// (1024 ** 3)
@@ -144,21 +82,16 @@ def update_disks():
         disk_canvases[i].delete("all")
         create_graph(disk_canvases[i], used/total*100, "yellow", 0, 5)
 
-    root.after(1000, update_disks)
+    root.after(info.disks_update_rate, update_disks)
 
 def update_nets():
-    disk_info = get_disks()
+    locip_label.config(text=f"    {info.get_local_ip()}")
+    extip_label.config(text=f"    {info.get_external_ip()}")
 
-    for i, disk in enumerate(disk_info):
-        used = (disk['total'] - disk['free'])// (1024 ** 3)
-        total = disk['total'] // (1024 ** 3)
+    tcp_label.config(text=f"TCP: {info.get_tcp_connections()} conns")
+    udp_label.config(text=f"UDP: {info.get_udp_connections()} conns")
 
-        disk_labels[i].config(text=f"{disk['root']}\\ {used} / {total} GB")
-
-        disk_canvases[i].delete("all")
-        create_graph(disk_canvases[i], used/total*100, "yellow", 0, 5)
-
-    root.after(1000, update_nets)
+    root.after(info.nets_update_rate, update_nets)
 
 
 root = Tk()
@@ -169,83 +102,64 @@ root.configure(background="black")
 root.wm_attributes("-transparentcolor", "black")
 root.wm_attributes("-topmost", 1)
 
+back_frame = Frame(root)
+back_frame.pack(side="bottom", fill="both", expand=True)
 
-ram_label = Label(root, text="RAM: x/x 100%", fg="white", font=("Helvetica Bold", 12, "bold"))
-ram_label.configure(background="black")
-ram_label.pack(side="top", anchor="w")
+label = Label(back_frame, text="TEXT")
+label.place(x=0, y=0)
 
+
+info = sysInfo()
+config = Config("config.json", default={
+    'position': [0,0],
+    'size': "220x280",
+    'save_pos': True
+}, debug=True)
+
+
+ram_label = default_label("RAM load percentage")
 ram_canvas = Canvas(root, width=root.winfo_width()+120, height=14, background="black")
 ram_canvas.pack(side="top", anchor="w")
-
 create_graph(ram_canvas, 50, "white", 0, 19)
 
-cpu_label = Label(root, text="CPU: 100%", fg="white", font=("Helvetica Bold", 12, "bold"))
-cpu_label.configure(background="black")
-cpu_label.pack(side="top", anchor="w")
-
+cpu_label = default_label("CPU load percentage")
 cpu_canvas = Canvas(root, width=root.winfo_width()+120, height=14, background="black")
 cpu_canvas.pack(side="top", anchor="w")
-
-create_graph(cpu_canvas, 80, "white", 0, 19)
-
+create_graph(cpu_canvas, 50, "white", 0, 19)
 
 disk_labels = []
 disk_canvases = []
-
-for i in range(len(psutil.disk_partitions())):
-    disk_label = Label(root, text="", fg="white", font=("Helvetica Bold", 12, "bold"))
-    disk_label.configure(background="black")
-    disk_label.pack(side="top", anchor="w")
-    disk_labels.append(disk_label)
-
+for i in range(len(info.get_disks())):
+    disk_labels.append( default_label("disk") )
     disk_canvas = Canvas(root, width=root.winfo_width()+120, height=3, background="black")
     disk_canvas.pack(side="top", anchor="w")
     disk_canvases.append(disk_canvas)
 
-locip = Label(root, text="locIP:", fg="white", font=("Helvetica Bold", 12, "bold"))
-locip.configure(background="black")
-locip.pack(side="top", anchor="w")
-locip_label = Label(root, text="    192.186.xx.xx", fg="white", font=("Helvetica Bold", 12, "bold"))
-locip_label.configure(background="black")
-locip_label.pack(side="top", anchor="w")
+locip = default_label("locIP:")
+locip_label = default_label("    local IP")
 
-extip = Label(root, text="extIP:", fg="white", font=("Helvetica Bold", 12, "bold"))
-extip.configure(background="black")
-extip.pack(side="top", anchor="w")
-extip_label = Label(root, text="    xx.xx.xx.xx", fg="white", font=("Helvetica Bold", 12, "bold"))
-extip_label.configure(background="black")
-extip_label.pack(side="top", anchor="w")
+extip = default_label("extIP:")
+extip_label = default_label("    external IP")
 
-tcp_label = Label(root, text="TCP: xxx conns", fg="white", font=("Helvetica Bold", 12, "bold"))
-tcp_label.configure(background="black")
-tcp_label.pack(side="top", anchor="w")
-
-udp_label = Label(root, text="UDP: xxx conns", fg="white", font=("Helvetica Bold", 12, "bold"))
-udp_label.configure(background="black")
-udp_label.pack(side="top", anchor="w")
+tcp_label = default_label("TCP ports")
+udp_label = default_label("UDP ports")
 
 
-config = "config.json"
-main_cfg = {
-    'position': [0,0],
-    'size': "220x280",
-    'save_pos': True
-}
 debug = True
 
 x2, xr2 = 0, True
 x1, xr1 = 0, True
 button_pressed = False
 
-init_config()
-cfg = read_config()
-last_pos = cfg['position']
-
-root.geometry(cfg['size'])
-root.geometry(f"+{cfg['position'][0]}+{cfg['position'][1]}")
+last_pos = config['position']
+last_saved_pos = config['position']
 
 if debug:
-    print(f"Initialized at {cfg['position']} with size {cfg['size']}")
+    print(f"Initialized at {config['position']} with size {config['size']}")
+
+
+root.geometry(config['size'])
+root.geometry(f"+{config['position'][0]}+{config['position'][1]}")
 
 root.bind('<Button-2>', clickwin)
 root.bind('<B2-Motion>', dragwin)
@@ -258,5 +172,6 @@ root.bind('<space>', hotkeys)
 update_cpuram()
 update_disks()
 update_nets()
+
 
 root.mainloop()
---
+++ utils.py
@@ -0,0 +1,157 @@
+import os
+import psutil
+import json
+
+
+class sysInfo:
+    def __init__(self, cpuram_update_rate: int = 100,
+            disks_update_rate: int = 100,
+            nets_update_rate: int = 100,
+            ):
+        self.cpuram_update_rate = cpuram_update_rate
+        self.disks_update_rate = disks_update_rate
+        self.nets_update_rate = nets_update_rate
+
+    def get_local_ip(self) -> str:
+        return "xx.xx.xx.xx"
+
+    def get_external_ip(self) -> str:
+        return "xx.xx.xx.xx"
+
+    def get_tcp_connections(self) -> int:
+        return 0
+
+    def get_udp_connections(self) -> int:
+        return 0
+
+    def get_disks(self) -> dict:
+        partitions = psutil.disk_partitions()
+        disk_info = []
+
+        for partition in partitions:
+            usage = psutil.disk_usage(partition.mountpoint)
+            disk_info.append({
+                'root': partition.device,
+                'total': usage.total,
+                'free': usage.free
+            })
+
+        return disk_info
+
+    def get_ram(self) -> int:
+        " RAM load percent "
+        return 50
+
+    def get_cpu(self) -> int:
+        " CPU load percent "
+        return 50
+
+    def get(self) -> dict:
+        return {
+            "extIP": self.get_external_ip(),
+            "locIP": self.get_local_ip(),
+            "tcpconns": self.get_tcp_connections(),
+            "udpconns": self.get_udp_connections(),
+            "disks": self.get_external_ip(),
+        }
+
+
+def config_file_exists_decorator(func):
+    def wrapper(self, *args, **kwargs):
+
+        if not os.path.isfile(self.file):
+            json.dump(self.data, open(self.file, 'w'), indent=self.indent)
+            print(f'Initialized default config file at {self.file}')
+
+        return func(self, *args, **kwargs)
+
+    return wrapper
+
+
+class Config:
+    def __init__(self, filepath: str, default: dict = {}, indent: str = 4, debug: bool = False, save_last: bool = True):
+        self.file = filepath
+        self.default = default
+        self.indent = indent
+        self.debug = debug
+        self.last_data = {}
+        self.save_last = save_last
+
+        if not os.path.isfile(self.file):
+            json.dump(self.data, open(self.file, 'w'), indent=self.indent)
+            print(f'Initialized default config file at {self.file}')
+
+
+    @config_file_exists_decorator
+    def change(self, **kwargs):
+        content = json.load( open(self.file, 'r') )
+        first = json.load( open(self.file, 'r') )
+
+        for key, value in kwargs.items():
+            content[key] = value
+
+        json.dump(content, open(self.file, 'w'), indent=self.indent)
+
+        if self.debug:
+            for key, value in first.items():
+                if not key in content.keys():
+                    print(f"Added new key: {key}={value}")
+                else:
+                    if value != content[key]:
+                        print(f"Changed value for key {key}, from {value} to {content[key]}")
+
+            for key, value in content.items():
+                if not key in first.keys():
+                    print(f"Removed key: {key}={value}")
+        if self.save_last:
+            self.last_data = json.load( open(self.file, 'r') )
+
+    @config_file_exists_decorator
+    def move_to(self, filepath: str):
+        data = self.data
+        last_path =  self.file
+        self.file = filepath
+
+        if os.path.isfile(last_path):
+            os.remove(last_path)
+
+        json.dump(data, open(self.file, 'w'), indent=self.indent)
+
+        if self.save_last:
+            self.last_data = json.load( open(self.file, 'r') )
+        if self.debug:
+            print(f"Moved file from {last_path} ro {self.file}")
+
+    @config_file_exists_decorator
+    def read(self, keys: list = []) -> dict:
+        if keys:
+            return { key: value for key, value in json.load( open(self.file, 'r') ).items() if key in keys }
+
+        if self.save_last:
+            self.last_data = json.load( open(self.file, 'r') )
+            return dict(self.last_data)
+        else:
+            return dict(json.load( open(self.file, 'r') ))
+
+    @property
+    def data(self) -> dict:
+        return self.read()
+    
+
+    def __str__(self):
+        return str(self.data)
+
+    def __dict__(self):
+        return self.data
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def __list__(self):
+        return self.data.keys()
+
+    def __repr__(self):
+        return f"Config(file={self.file}, data={self.data})"
+
+
+if __name__ == '__main__':
+    print(sysInfo().get())
